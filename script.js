199fed54c8485a1fa3ba903cfda673fa7998ee47
RagaviShanmugaraja <160751192+RagaviShanmugaraja@users.noreply.github.com>
2026-04-21 13:20:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let currentPage = 0;
+const pages = document.querySelectorAll(".page");
+
+function nextPage() {
+    pages[currentPage].classList.remove("active");
+    currentPage++;
+    pages[currentPage].classList.add("active");
+}
+
+// 🎵 Music control
+function playMusic() {
+    const music = document.getElementById("bg-music");
+    music.play();
+}
+
+// Slideshow
+const images = [
+    <><img src="RRimage1.jpg"></img><img src="RRimage2.jpg"></img><img src="RRimage3.jpg"></img><img src="RRimage4.jpg"></img></>
+    
+];
+
+let index = 0;
+const slideshow = document.getElementById("slideshow");
+
+setInterval(() => {
+    if (slideshow) {
+        index = (index + 1) % images.length;
+        slideshow.src = images[index];
+    }
+}, 2000);
